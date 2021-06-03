79b42d20dcf502842b32f4fbf182283916391807
Marcus Baltazar <mvabf_dev@outlook.com>
2021-06-03 13:27:34 -0300

    feat: auth feature

--- src/database/migrations/1621098152331-CreateUsers.ts
+++ src/database/migrations/1621098152331-CreateUsers.ts
@@ -18,6 +18,10 @@ export class CreateUsers1614728289743 implements MigrationInterface {
         type: 'varchar'
       },
       {
+        name: 'password',
+        type: 'varchar'
+      },
+      {
         name: 'created_at',
         type: 'timestamp',
         default: 'now()'
--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,21 @@
-import {Router}  from 'express';
+import { Router } from 'express';
+
+import authMiddleware from "./middlewares/authMiddleware";
+
 import { AccountController } from './controllers/AccountController';
 import { ExpensesController } from './controllers/ExpensesController';
 import { IncomingController } from './controllers/IncomingController';
 import { UserController } from './controllers/UserController';
+import { AuthController } from './controllers/AuthController';
 
-const routes =  Router()
+const routes = Router()
 const usersController = new UserController()
 const expenseController = new ExpensesController()
 const incomingsController = new IncomingController()
 const accountController = new AccountController()
+const authController = new AuthController()
 
-routes.get('/users', usersController.show)
+routes.get('/users', authMiddleware, usersController.show)
 routes.post('/users', usersController.createUser)
 
 routes.get('/expenses', expenseController.show)
@@ -21,6 +26,8 @@ routes.post('/incomings', incomingsController.createIncoming)
 
 routes.get('/accounts', accountController.showAccounts)
 routes.post('/accounts', accountController.createAccount)
+
+routes.post('/auth', authController.authenticate)
 
 
 export {routes}
